de582935acb59baf129e913452b4371c753adb6a
ImJustChew <chewtzihwee@gmail.com>
2025-03-04 21:07:38 +0800

    initialize project with Hono framework, add calendar API, and set up testing

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
+
+import acaCalendar from './aca-calendar'
+
+export const app = new Hono()
+  .use(cors({
+    origin: process.env.NODE_ENV === 'production' ? 'https://nthumods.com' : '*',
+  }))
+  .use(logger())
+  .get('/', (c) => {
+    return c.text('Hello Hono!')
+  })
+  .route('/acacalendar', acaCalendar)
+  
+const port = parseInt(process.env.PORT!) || 3000
+
+export default {
+  port: port,
+  fetch: app.fetch
+}
